a29ff81516c1bba91e7e2cdb15beb9c34ac0b67c
Miguel-Angel-Mejia-Antonio <73071287+Miguel-Angel-Mejia-Antonio@users.noreply.github.com>
2021-05-19 09:03:18 -0500

    Add files via upload

---
+++ Examen/puntero.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	//Paso 1
+	char x;
+	char *p;
+
+	//Paso 2
+	char y;
+	char *puntero;
+
+	//Paso 3
+	x='x';
+
+	//Paso 4
+	p=&x;
+
+	//Paso 5
+	printf("Contenido del puntero [p]= %c \n", *p);
+
+	//Paso 6
+	p=p+1;
+
+	//Paso 7
+	printf("Contenido del puntero [p]= %c \n", *p);
+
+	//Paso 8
+	p=p+2;
+
+	//Paso 9
+	printf("Contenido del puntero [p]= %c \n", *p);
+
+	//Paso 10
+	y='y';
+
+	//Paso 11
+	puntero=&y;
+
+	//Paso 12
+	printf("Contenido del puntero [puntero]= %c \n", *puntero);
+
+
+
+	return 0;
+
+}
