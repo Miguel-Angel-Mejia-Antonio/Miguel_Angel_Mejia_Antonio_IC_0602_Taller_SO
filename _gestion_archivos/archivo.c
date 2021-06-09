186136127aa43c03b21330e9ff8a9a9f5c0b174e
Miguel-Angel-Mejia-Antonio <73071287+Miguel-Angel-Mejia-Antonio@users.noreply.github.com>
2021-06-08 23:59:50 -0500

    Update archivo.c

--- _gestion_archivos/archivo.c
+++ _gestion_archivos/archivo.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+//Creamos una estructura en donde almacenaremos la palabra
 struct cont_palabras{
     char palabras[500];
 }palabra[1000];
 
 int main(int argc, char *argv[]){
-
+	//Declaramos un FILE de nombre archivo para pedir el primer argumento
 	FILE*archivo = fopen(argv[1],"r");
 
     int i,j;
     int contador = 0;
     char aux[100];
-
+    //Comprobamos que nuestro archivo de entrada exista
     if(archivo == NULL)
     {
         printf("No se ha podido abrir el archivo.\n");
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]){
         printf("--ARCHIVO ---->%s<---- ABIERTO CORRECTAMENTE--\n",argv[1]);
         printf("--------------------------------------------------------\n\n");
     }
-
+    //Leemos las palabras que tengamos en el primer archivo del argumento que hemos pedido
     for(i=0; !feof(archivo); i++)
     {
         fgets(palabra[i].palabras,100,archivo);
@@ -34,6 +34,8 @@ int main(int argc, char *argv[]){
     printf("--------------------------------------------------------\n");
     printf("---------ORDENANDO PALABRAS----------\n");
     printf("--------------------------------------------------------\n\n");
+	
+    //Ordenamos las palabras por orden alfabético
     for(i=0; i<contador; i++)
     {
         for(j=0; j<contador; j++)
@@ -47,17 +49,19 @@ int main(int argc, char *argv[]){
         }
     }
     
+    //Cerramos nuestro archivo
     printf("--------------------------------------------------------\n");
     printf("--CERRANDO EL ARCHIVO: -->%s<----\n",argv[1]);
     printf("--------------------------------------------------------\n\n");
     fclose(archivo);
 
+    //Abrimos el argumento de salida donde se almacenaran nuestras palabras guardadas   
     fopen(argv[2],"w");
-
     printf("--------------------------------------------------------\n");
     printf("--PALABRAS ORDENADAS CORRECTAMENTE en -->%s<----\n",argv[2]);
     printf("--------------------------------------------------------\n");
 
+    //Imprimimos nuestras palabras en el archivo de salida y cerramos el archivo
     for(i=0;i<=contador;i++)
     {
         fprintf(archivo,"%s",palabra[i].palabras);
